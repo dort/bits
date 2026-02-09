06f33b20575ef6b9f463abb5126dc908396eb1dc
charlie derr <charlie@nonlanguage.dev>
2026-02-09 17:20:08 -0500

    only accurate on the lower half of the probability range

--- sam-tutorial/hdc/rtm2.py
+++ sam-tutorial/hdc/rtm2.py
@@ -9,21 +9,21 @@ for metric = e, the probability will be equal for 0s and 1s
 for metric > e, density of 1s will be greater than .5
 '''
 def sc(metric = math.e) :
-    density = math.log(metric-1.0)
+    density = 0.5 * (math.log(metric))
     print(metric, density)
     return density
 
 
 
-def random_tuple(len=3000, sparsity=0.5):
+def random_tuple(length=3000, sparsity=0.5):
 
-    result = tuple(random.choices([0, 1], weights=[1.0-sparsity, sparsity], k=len))
+    result = tuple(random.choices([0, 1], weights=[1.0-sparsity, sparsity], k=length))
     
     return result
 
 
 def multiple_RTs(num=10):
-    result = [random_tuple(len=10, sparsity=sc(3 )) for _ in range(num)]
+    result = [random_tuple(length=10, sparsity=sc(math.e)) for _ in range(num)]
     return result
 
 
