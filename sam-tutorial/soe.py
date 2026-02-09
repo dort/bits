5e1d91451894b5c429c291e3df03ae1a8762d1b0
charlie derr <charlie@nonlanguage.dev>
2026-02-09 17:50:50 -0500

    not sure where we left off here

--- sam-tutorial/soe.py
+++ sam-tutorial/soe.py
@@ -1,13 +1,13 @@
+#! /usr/bin/python3
 # Sieve of Eratoshtenes
 
 import math
 from pprint import pprint as pp
 
 # prime finder
-
-
 def pf(upper_bound=1000):
     primes = []
+    
     for i in range(2, upper_bound + 1):
         is_prime = True
         for j in primes:
@@ -16,11 +16,19 @@ def pf(upper_bound=1000):
             if i % j == 0:
                 is_prime = False
                 break
+
+            
         if is_prime:
             primes.append(i)
+            
+
+            
     return primes
 
 
+
+
 if __name__ == "__main__":
-    print(pf())
+    res=pf(10000)
+    print(res, len(res))
 
