ee8e71c782191cf1e40490505a9a17c76fe46a81
charlie derr <charlie@nonlanguage.dev>
2026-02-09 10:12:21 -0500

    incremental -- still buggy as all getout

---
+++ sam-tutorial/hdc/rtm2.py
@@ -0,0 +1,40 @@
+import random
+import math
+
+
+'''sparsity converter
+metric input must be 1 or greater
+for values of metric between 1 and e, density of 1s will be less than .5
+for metric = e, the probability will be equal for 0s and 1s
+for metric > e, density of 1s will be greater than .5
+'''
+def sc(metric = math.e) :
+    density = math.log(metric-1.0)
+    print(metric, density)
+    return density
+
+
+
+def random_tuple(len=3000, sparsity=0.5):
+
+    result = tuple(random.choices([0, 1], weights=[1.0-sparsity, sparsity], k=len))
+    
+    return result
+
+
+def multiple_RTs(num=10):
+    result = [random_tuple(len=10, sparsity=sc(3 )) for _ in range(num)]
+    return result
+
+
+def main():
+    a = multiple_RTs()
+    for  b in a :
+        print(b)
+
+
+if __name__ == "__main__":
+    main()
+
+
+                           
